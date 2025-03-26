a4627c5d34e40eca0829ef6ce698dd4492d483da
Jerry Endaya <vagabondendaya@gmail.com>
2025-03-26 17:07:37 +0800

    changing the localhost to ip address and removing commented code in the product service

--- streamline-frontend/src/service/API.tsx
+++ streamline-frontend/src/service/API.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "localhost:8080/api/v1/auth",
+    baseURL: "http://127.0.0.1:8080/api/v1/auth",
     withCredentials: true, 
 });
 
