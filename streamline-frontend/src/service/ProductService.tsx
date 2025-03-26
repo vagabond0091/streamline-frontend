df77e201b75923f494af59eb3ce729101d9c2485
Jerry Endaya <vagabondendaya@gmail.com>
2025-03-26 17:05:50 +0800

    Added a bearer token in the product service

--- streamline-frontend/src/service/ProductService.tsx
+++ streamline-frontend/src/service/ProductService.tsx
@@ -1,7 +1,19 @@
 
+
 import API from "./API";
+import CookieService from "./CookieService";
+const token = CookieService.getToken()?.trim();
 const ProductService = {
-    createProduct: async (product:any) => await API.post( "/create",product),
+    // createProduct: async (product: any) => {
+    //     const token = localStorage.getItem("jwt_token"); // Ensure token is retrieved here
+    //     if (!token) {
+    //         console.error("No token found! User may not be authenticated.");
+    //         return;
+    //     }
+    //     console.log(token);
+    // }
+    createProduct: async (product:any) => await API.post( "/create",product,{
+        headers: { Authorization: `Bearer ${token}`,"Content-Type": "application/json" },}),
     
 
 }
